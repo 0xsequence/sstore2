03ae7961e724ede9768ca61a97a4de815389fbf6
Agustin Aguilar <Agusxrun@gmail.com>
2021-10-07 18:16:18 +0000

    Add NatSpec

--- contracts/SSTORE2Map.sol
+++ contracts/SSTORE2Map.sol
@@ -6,6 +6,12 @@ import "@0xsequence/create3/contracts/Create3.sol";
 import "./utils/Bytecode.sol";
 
 
+/**
+  @title A write-once key-value storage for storing chunks of data with a lower write & read cost.
+  @author Agustin Aguilar <aa@horizon.io>
+
+  Readme: https://github.com/0xsequence/sstore2#readme
+*/
 library SSTORE2Map {
   error WriteError();
 
@@ -18,10 +24,24 @@ library SSTORE2Map {
     return keccak256(abi.encode(SLOT_KEY_PREFIX, _key));
   }
 
+  /**
+    @notice Stores `_data` and returns `pointer` as key for later retrieval
+    @dev The pointer is a contract address with `_data` as code
+    @param _data To be written
+    @param _key unique string key for accessing the written data (can only be used once)
+    @return pointer Pointer to the written `_data`
+  */
   function write(string memory _key, bytes memory _data) internal returns (address pointer) {
     return write(keccak256(bytes(_key)), _data);
   }
 
+  /**
+    @notice Stores `_data` and returns `pointer` as key for later retrieval
+    @dev The pointer is a contract address with `_data` as code
+    @param _data to be written
+    @param _key unique bytes32 key for accessing the written data (can only be used once)
+    @return pointer Pointer to the written `_data`
+  */
   function write(bytes32 _key, bytes memory _data) internal returns (address pointer) {
     // Append 00 to _data so contract can't be called
     // Build init code
@@ -36,26 +56,68 @@ library SSTORE2Map {
     pointer = Create3.create3(internalKey(_key), code);
   }
 
+  /**
+    @notice Reads the contents for a given `_key`, it maps to a contract code as data, skips the first byte
+    @dev The function is intended for reading pointers first written by `write`
+    @param _key string key that constains the data
+    @return data read from contract associated with `_key`
+  */
   function read(string memory _key) internal view returns (bytes memory) {
     return read(keccak256(bytes(_key)));
   }
 
+  /**
+    @notice Reads the contents for a given `_key`, it maps to a contract code as data, skips the first byte
+    @dev The function is intended for reading pointers first written by `write`
+    @param _key string key that constains the data
+    @param _start number of bytes to skip
+    @return data read from contract associated with `_key`
+  */
   function read(string memory _key, uint256 _start) internal view returns (bytes memory) {
     return read(keccak256(bytes(_key)), _start);
   }
 
+  /**
+    @notice Reads the contents for a given `_key`, it maps to a contract code as data, skips the first byte
+    @dev The function is intended for reading pointers first written by `write`
+    @param _key string key that constains the data
+    @param _start number of bytes to skip
+    @param _end index before which to end extraction
+    @return data read from contract associated with `_key`
+  */
   function read(string memory _key, uint256 _start, uint256 _end) internal view returns (bytes memory) {
     return read(keccak256(bytes(_key)), _start, _end);
   }
 
+  /**
+    @notice Reads the contents for a given `_key`, it maps to a contract code as data, skips the first byte
+    @dev The function is intended for reading pointers first written by `write`
+    @param _key bytes32 key that constains the data
+    @return data read from contract associated with `_key`
+  */
   function read(bytes32 _key) internal view returns (bytes memory) {
     return Bytecode.codeAt(Create3.addressOf(internalKey(_key)), 1, type(uint256).max);
   }
 
+  /**
+    @notice Reads the contents for a given `_key`, it maps to a contract code as data, skips the first byte
+    @dev The function is intended for reading pointers first written by `write`
+    @param _key bytes32 key that constains the data
+    @param _start number of bytes to skip
+    @return data read from contract associated with `_key`
+  */
   function read(bytes32 _key, uint256 _start) internal view returns (bytes memory) {
     return Bytecode.codeAt(Create3.addressOf(internalKey(_key)), _start + 1, type(uint256).max);
   }
 
+  /**
+    @notice Reads the contents for a given `_key`, it maps to a contract code as data, skips the first byte
+    @dev The function is intended for reading pointers first written by `write`
+    @param _key bytes32 key that constains the data
+    @param _start number of bytes to skip
+    @param _end index before which to end extraction
+    @return data read from contract associated with `_key`
+  */
   function read(bytes32 _key, uint256 _start, uint256 _end) internal view returns (bytes memory) {
     return Bytecode.codeAt(Create3.addressOf(internalKey(_key)), _start + 1, _end + 1);
   }
